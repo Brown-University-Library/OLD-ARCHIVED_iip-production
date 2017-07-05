e0cb24ec955a9b34cb10ce8fa828b7c5246a48cd
birkin <birkin.diana@gmail.com>
2017-07-05 09:10:35 -0400

    enables required import.

--- iip_smr_config/urls.py
+++ iip_smr_config/urls.py
@@ -2,7 +2,7 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
-# from django.views.generic import RedirectView
+from django.views.generic import RedirectView
 from iip_smr_web_app import views
 
 
