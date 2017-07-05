d7ee767bbfd53fc7e588ef775f1dbb1431f07b17
birkin <birkin.diana@gmail.com>
2017-07-05 09:05:57 -0400

    updates environ-load syntax.

--- iip_smr_web_app/settings_app.py
+++ iip_smr_web_app/settings_app.py
@@ -7,21 +7,21 @@ import json, os
 
 ## solr ##
 
-SOLR_URL = os.environ['IIP_SEARCH__SOLR_URL']  # main solr instance
+SOLR_URL = os.environ['IIP_SMR__SOLR_URL']  # main solr instance
 
 
 ## auth ##
 
-DEV_AUTH_HACK = os.environ[u'IIP_SEARCH__DEV_AUTH_HACK']  # 'enabled' or 'disabled' (only enabled for local non-shib development)
-LEGIT_ADMINS = json.loads( os.environ['IIP_SEARCH__LEGIT_ADMINS'] )  # json shib-eppn list
-DB_USER = os.environ['IIP_SEARCH__DB_USER']
-DB_USER_PASSWORD = os.environ['IIP_SEARCH__DB_USER_PASSWORD']
+DEV_AUTH_HACK = os.environ[u'IIP_SMR__DEV_AUTH_HACK']  # 'enabled' or 'disabled' (only enabled for local non-shib development)
+LEGIT_ADMINS = json.loads( os.environ['IIP_SMR__LEGIT_ADMINS'] )  # json shib-eppn list
+DB_USER = os.environ['IIP_SMR__DB_USER']
+DB_USER_PASSWORD = os.environ['IIP_SMR__DB_USER_PASSWORD']
 
 ## misc ##
 
-XML_DIR_PATH = os.environ['IIP_SEARCH__XML_DIR_PATH']
+XML_DIR_PATH = os.environ['IIP_SMR__XML_DIR_PATH']
 
-URL_SCHEME = os.environ['IIP_SEARCH__URL_SCHEME']  # 'http' or, on production, 'https'
+URL_SCHEME = os.environ['IIP_SMR__URL_SCHEME']  # 'http' or, on production, 'https'
 
 
 ## end
