0d7daa9a3265cd662333a9cf2d03ffc2bed13b0d
WennieZ <zhang.wennie@gmail.com>
2017-07-27 10:40:40 -0400

    can edit

--- iip_smr_web_app/views.py
+++ iip_smr_web_app/views.py
@@ -19,7 +19,7 @@ log = logging.getLogger(__name__)
 
 
 def temp( request ):
-    message = 'hellooo %s' % str( datetime.datetime.now() )
+    message = 'helloooPPPPP %s' % str( datetime.datetime.now() )
     log.debug( 'test log debug entry' )
     log.info( 'test log info entry' )
     log.error( 'test log error entry' )
