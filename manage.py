7a86ea6ce96e014bf415b672d449bb264012fe7f
AllenZYoung <allenzyoung97@gmail.com>
2019-02-05 15:14:30 -0500

    first commit by yang

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-
+# first commit by Yang
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iip_smr_config.settings")
-
+    print('Hi there')
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
