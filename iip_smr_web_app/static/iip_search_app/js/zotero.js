f6e0541f367c861b7a0cfe1ea3e8cd267af45514
AllenZYoung <allenzyoung97@gmail.com>
2019-02-19 14:33:11 -0500

    Version 1.1, for full entry issue

--- iip_smr_web_app/static/iip_search_app/js/zotero.js
+++ iip_smr_web_app/static/iip_search_app/js/zotero.js
@@ -7,6 +7,7 @@ function add_citation(ztag) {
     if (ztag === "ms") {
         targets.html("Supplied by Michael Satlow.");
     }
+    console.log(ztag);
 
 
     var req = new XMLHttpRequest();
@@ -17,8 +18,16 @@ function add_citation(ztag) {
         targets.html("");
         if (bibs.length > 0) {
             bibs.forEach(function(b) {
-                targets.append(b.bib);
-                $(".csl-entry", targets).append(" (<a href='"+b.links.alternate.href+"' target='_blank'>Full Entry</a>)");
+                console.log(Object.entries(b));
+                targets.append(b.bib);   
+                $(".csl-entry", targets).css('text-decoration', 'underline');
+                $(".csl-entry", targets).css('color', 'blue');
+                $(".csl-entry", targets).css( 'cursor', 'pointer' );
+                $(".csl-entry", targets).on('click', function() {
+                    location.href = b.links.alternate.href;
+                        $(this).css("color", "red");
+ 
+                });
             });
         } else {
             targets.innerHTML = "Not found: " + ztag;
@@ -26,15 +35,15 @@ function add_citation(ztag) {
 
         targets.each(function(i,d) {
             d = $(d);
-            var ntype = d.attr("ntype");
-            var n = d.attr("n");
+            var ntype = d['context']["ntype"];
+            var n = d['context']["n"];
             if (ntype) {
                 if (ntype === "page") {
-                    $(".csl-entry", d).append(" (p." + n + ")");
+                    $(".csl-entry", d).append(" <span>(p." + n + ")</span>");
                 } else if (ntype === "insc") {
-                    $(".csl-entry", d).append(" (inscription " + n + ")");
+                    $(".csl-entry", d).append("<span>(inscription " + n + ")</span>");
                 } else {
-                    $(".csl-entry", d).append(" ("+ntype + " " + n + ")");
+                    $(".csl-entry", d).append("<span>("+ntype + " " + n + ")</span>");
                 }
             }
         });
