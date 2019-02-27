df4021ff6ddb9ef9879231b3642c06d6a6139f97
AllenZYoung <allenzyoung97@gmail.com>
2019-02-27 11:43:45 -0500

    Update zotero.js to match the APA format

--- iip_smr_web_app/static/iip_search_app/js/zotero.js
+++ iip_smr_web_app/static/iip_search_app/js/zotero.js
@@ -104,8 +104,7 @@
 //     });
 // });
 
-//User 1604128 is IIP
-var user = "1604128";
+
 //Collection U2J49649 is also called IIP
 var collection = "U2J49649";
 //IIP Group
@@ -113,8 +112,9 @@ var GROUP = "180188";
 
 function retrieve_bib(id_list, callback) {
   var tags = id_list.join(" || ");
+  console.log(tags);
   var req = new XMLHttpRequest();
-  req.open("GET", "https://api.zotero.org/groups/" + GROUP + "/items?tag=" + tags + "&format=atom&content=bib,json", true);
+  req.open("GET", "https://api.zotero.org/groups/" + GROUP + "/items?tag=" + tags + "&content=bib,json" + "&style=apa", true);
   req.setRequestHeader("Zotero-API-Version", "3");
   req.onload = callback;
   req.send();
@@ -132,38 +132,51 @@ function render_bibliography() {
       id_list.push(new_id);
     }
   }
+
   retrieve_bib(id_list, function () {
     if (this.status == 200) {
-      var data = this.responseXML;
-      var entries = data.documentElement.getElementsByTagName('entry');
+      var responseXML = this.responseXML;
+      console.log("DEBUG:\tresponseXML", responseXML);
+      var entries = responseXML.documentElement.getElementsByTagName('entry');
+      console.log("DEBUG:\tentries[0]", entries[0]);
+
       for (var i = 0; i < entries.length; i++) {
         var contents;
-        var contents_f = entries[i].getElementsByTagName("content")[0];
+        var contents_f = entries[i].getElementsByTagName("content")[0]; // bib
+        console.log('DEBUG\tcontents_f', contents_f);
         if (contents_f.children) {
           contents = contents_f.children;
         } else {
           contents = contents_f.getElementsByTagName("subcontent");
         }
         var entryjson = JSON.parse(contents[1].textContent);
+        console.log("contents[0]", contents[0]);
+        console.log("DEBUG:\tentryjson.archiveLocation", entryjson.archiveLocation);
         bibliographies[entryjson.archiveLocation] = {};
         bibliographies[entryjson.archiveLocation].parsed = entryjson;
         bibliographies[entryjson.archiveLocation].full = contents[0].textContent;
         bibliographies[entryjson.archiveLocation].url = entries[i].getElementsByTagName("id")[0].textContent;
+        bibliographies[entryjson.archiveLocation].content = contents_f.getElementsByClassName('csl-bib-body')[0].innerHTML;
+        console.log("DEBUG:\t", bibliographies[entryjson.archiveLocation].content);
+
       }
 
+      // this is for bibls
       $("li.biblToRetrieve").each(function () {
 
         var bspan = $(this).find('span')[0];
         b = bspan.innerHTML.trim();
-
+        console.log("DEBUG:\tb", b);
         var pages = $(this).find('ul li');
         var new_html;
 
         try {
-          new_html = bibliographies[b].full + "<br/>";
+          new_html = bibliographies[b].content;
+          console.log("DEBUG:\tnewhtml", new_html);
+
         }
         catch (err) {
-          console.log('Catch error here 1!')
+          console.log('WARNING:\tCatch error here 1!')
           new_html = b + " (Citation not found in Zotero!)";
           bspan.innerHTML = new_html;
           pages.each(function () {
@@ -180,39 +193,43 @@ function render_bibliography() {
         this.attributes.class.value = "";
         if (pages.length !== 0) new_html += "(";
         var semicolon = "; ";
-        console.log(new_html);
+        console.log("DEBUG:\tnewhtml", new_html);
         for (var i = 0; i < pages.length; i++) {
           if (i == pages.length - 1) {
             semicolon = "";
           }
-          console.log(pages[i]);
+          console.log("DEBUG:\tpages[i]", pages[i]);
           var entry = pages[i].innerHTML.split("|");
-          console.log(entry);
           entry = entry[0].replace(/\s+/g, '').split('.');
-          console.log(entry);
+          console.log("DEBUG:\tentry",entry);
           if (entry[0] == 'page' || entry[0] == 'p') {
             new_html += "Page. " + entry[1] + semicolon;
           } else {
-            new_html += "Insc." + entry[1] + semicolon;
-            console.log('new_html', new_html);
+            if (entry.length === 1 ) 
+              new_html += "Insc." + entry[0] + semicolon;
+            else {
+              new_html += "Insc." + entry[1] + semicolon;
+            }
+            console.log('DEBUG:\tnew_html', new_html);
           }
         }
         if (pages.length != + 0) {
           new_html += ")<br/>";
         }
-        console.log($(this).find("ul")[0]);
+        console.log("DEBUG:\t", $(this).find("ul")[0]);
         $(this).find("ul")[0].innerHTML = "";
-        if (bibliographies[b].url) new_html += "<a style='display:inline;' href='" + bibliographies[b].url + "'>Link to Full Entry</a>";
+        if (bibliographies[b].url) 
+          new_html += "<a style='display:inline;' href='" + bibliographies[b].url + "'>(Link to Full Entry)</a>";
 
         bspan.innerHTML = new_html;
-        console.log('bspan.innerHTML', bspan.innerHTML);
+        console.log('DEBUG:\tbspan.innerHTML', bspan.innerHTML);
 
       });
 
-
+      // this is for upper threee parts
       $("span.biblToRetrieve").each(function () {
         var b = this.innerHTML.split("|");
-        console.log('Alaaaaaaaska');
+        console.log('DEBUG:\t b');
         console.log(b);
         b_array = Array()
         split_substring = "', '"
@@ -222,15 +239,18 @@ function render_bibliography() {
         b_array[0] =  b[0].slice(2, idx_firstsplit);
         b_array[1] =  b[0].slice(idx_firstsplit + split_substring.length, idx_secondsplit);
         b_array[2] =  b[0].slice(idx_secondsplit + split_substring.length, idx_endsplit);
-        console.log('Hawaaaaaaaaai');
+        console.log('HaDEBUG:\t b_array ');
         console.log(b_array);
         b = b_array;
         try {
           var entry = bibliographies[b[0]].parsed;
           var colon = ": ";
           if (b[2] === "") colon = "";
+          // this.innerHTML = "<div>" +
+          // entry.creators[0].lastName + ". " + "<i>" + entry.title + "</i>" + ", " + entry.date + colon + '(' + b[1] + '.' +  b[2] + ')' + " (<a href='" + bibliographies[b[0]].url + "'>Full</a>)"
+          // +"</div>";
           this.innerHTML = "<div>" +
-          entry.creators[0].lastName + ". " + entry.title + ", " + entry.date + colon + '(' + b[1] + '.' +  b[2] + ')' + " (<a href='" + bibliographies[b[0]].url + "'>Full</a>)"
+          entry.creators[0].lastName + ". " + "<i>" + entry.title + "</i>" + ", " + entry.date + colon + '(' + b[1] + '.' +  b[2] + ')' + " (<a href='" + bibliographies[b[0]].url + "'>Full</a>)"
           +"</div>";
         }
         catch (err) {
@@ -246,7 +266,7 @@ function render_bibliography() {
               txt += "Inscription " + b[2];
             }
             this.innerHTML = txt + " (Citation Not Found)";
-            console.log("this.innerHTML", this.innerHTML);
+            console.log("DEBUG:\tthis.innerHTML", this.innerHTML);
           }
         }
         this.attributes.class.value = "";
